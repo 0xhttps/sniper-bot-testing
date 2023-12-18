d068e5c6f25d450456d7cafdd7b8e629bba94ed0
0xhttps <mjquinn11@gmail.com>
2023-12-17 22:59:50 -0800

    ✔️

--- index.ts
+++ index.ts
@@ -7,4 +7,4 @@ const private_key = process.env.PRIVATE_KEY;
 const contract_addr = "0x36e16e278aAE09808EC7E0F9F512d54a2E976d3D";
 const account_addr = "0xb00b62c7160B2d37D026B3B5bfe0667289E77C5C";
 
-contractInteraction(contract_addr, account_addr, private_key);
+contractInteraction(contract_addr, account_addr, private_key, "mint", "10");
--- util/contract_interaction.ts
+++ util/contract_interaction.ts
@@ -6,24 +6,47 @@ require("dotenv").config();
 export async function contractInteraction(
   contract_addr: string,
   account_addr: string,
-  PRIVATE_KEY: any
+  PRIVATE_KEY: any, 
+  function_name: any,
+  token_id: any
 ) {
+
   const signer = new ethers.Wallet(PRIVATE_KEY, provider);
   const abi = await getAbi(contract_addr);
   const contract = new ethers.Contract(contract_addr, abi, signer);
   const contractFunctions = mapContractFunctions(abi);
+  let availableFunctions = '';
+  let i = 1
 
-  const param_to = account_addr;
-  const param_tokenId = "6";
+  for (const f of contractFunctions as ContractFunction[]) {
+    if (f.name) {
+      availableFunctions += `${f.name}, `;
+    }
+  }
 
-  await contract[contractFunctions[2]["name"]](param_to, param_tokenId)
-    .then((tx: any) => {
-      console.log("Transaction sent: ", tx.hash);
-      return tx.hash;
-    })
-    .catch((error: any) => {
-      console.error("Error sending transaction: ", error.info);
-      return error;
-    });
+  for (i; i < contractFunctions.length; i++) {
+    if ((contractFunctions[i].name).toLowerCase().includes(function_name.toLowerCase())) {
+      await contract[function_name](account_addr, token_id)
+        .then((tx: any) => {
+          console.log("Transaction sent: ", tx.hash);
+          return tx.hash;
+        })
+        .catch((error: any) => {
+          console.error("Error sending transaction: ", error.info);
+          return error;
+        });
+        break
+    }
+  } 
+  if (i === contractFunctions.length) {
+    console.error(`Function "${function_name}" not found in abi\nAvailable functions:\n${availableFunctions.slice(0, -2)}`);
+  }
 }
+
+type ContractFunction = {
+  name?: string;
+  parameters: any[];
+  stateMutability: string;
+};
+
 export {};
